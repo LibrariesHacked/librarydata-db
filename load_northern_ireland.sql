a32e7408b4895e5f916438af78f5ab90268a6afe
Dave <DaveBathnes@users.noreply.github.com>
2020-09-09 21:30:34 +0100

    #6 Completing NI opening hours. Closes #6

--- load_northern_ireland.sql
+++ load_northern_ireland.sql
@@ -4,11 +4,19 @@ create table schemas_staging_northern_ireland (
   address_2 character varying (250),
   address_3 character varying (250),
   postcode character varying (9),
-  url text
+  url text,
+  email text,
+  monday text, 
+  tuesday text, 
+  wednesday text,
+  thursday text,
+  friday text,
+  saturday text,
+  sunday text
 );
 
 -- Scotland Libraries dataset
-\copy schemas_staging_northern_ireland from 'data/libraries_northern_ireland.csv' csv header force null address_1,address_2,address_3,postcode,url;
+\copy schemas_staging_northern_ireland from 'data/libraries_northern_ireland.csv' csv header force null address_1,address_2,address_3,postcode,url,email,monday,tuesday,wednesday,thursday,friday,saturday,sunday;
 
 -- Use standard postcode
 update schemas_staging_northern_ireland lu
@@ -17,7 +25,7 @@ from geo_postcode_lookup p
 where replace(p.postcode, ' ', '') =  replace(lu.postcode, ' ', '');
 
 -- Load the real libraries table
-insert into schemas_libraries (local_authority_code, name, address_1, address_2, address_3, postcode, statutory, colocated, library_type_id, url)
+insert into schemas_libraries (local_authority_code, name, address_1, address_2, address_3, postcode, statutory, colocated, library_type_id, monday_staffed_hours, tuesday_staffed_hours, wednesday_staffed_hours, thursday_staffed_hours, friday_staffed_hours, saturday_staffed_hours, sunday_staffed_hours, url, email_address)
 select distinct
   'N92000002',
   st.name,
@@ -28,7 +36,15 @@ select distinct
   True as statutory,
   False as colocated,
   (select id from schemas_library_type where name = 'LAL') as library_type_id,
-  st.url
+  st.monday, 
+  st.tuesday, 
+  st.wednesday,
+  st.thursday,
+  st.friday,
+  st.saturday,
+  st.sunday,
+  st.url,
+  st.email
 from schemas_staging_northern_ireland st
 order by name;
 
--- load_temp.sql
+++
@@ -1,9 +0,0 @@
--- switch to using the database
-\c librarydata_temp;
-
--- set client encoding
-set client_encoding = 'UTF8';
-
-\i 'load_wales.sql';
-
-vacuum analyze;
