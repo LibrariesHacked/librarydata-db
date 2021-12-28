aacf3d1e16aa4a5f6e47f3ff360cda1a0f9ee4b0
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-12-28 21:59:53 +0000

    #14 Loading additional authority data for names. Closes #14

--- load.sql
+++ load.sql
@@ -6,7 +6,57 @@ create index idx_geouprn_xy on geo_uprn (x_coordinate, y_coordinate);
 \copy geo_postcode_lookup from 'data/ons_postcode_directory.csv' csv header force null easting,northing,latitude,longitude,lsoa,district,ward,county,region,country,date_of_termination;
 
 -- Local authorities
-\copy schemas_local_authority from 'data/ons_local_authorities.csv' csv header;
+create table schemas_staging_local_authority (
+  "local-authority-code" text,
+  "official-name" text,
+  "nice-name" text,
+  "gss-code" text,
+  "start-date" text,
+  "end-date" text,
+  "replaced-by" text,
+  "nation" text,
+  "region" text,
+  "local-authority-type" text,
+  "local-authority-type-name" text,
+  "county-la" text,
+  "combined-authority" text,
+  "alt-names" text,
+  "former-gss-codes" text,
+  "notes" text,
+  "current-authority" text,
+  "BS-6879" text,
+  "ecode" text,
+  "even-older-register-and-code" text,
+  "gov-uk-slug" text,
+  "area" text,
+  "pop-2020" text,
+  "x" text,
+  "y" text,
+  "long" text,
+  "lat" text,
+  "powers" text,
+  "lower_or_unitary" text,
+  "mapit-area-code" text,
+  "ofcom" text,
+  "old-ons-la-code" text,
+  "old-register-and-code" text,
+  "open-council-data-id" text,
+  "os-file" text,
+  "os" text,
+  "snac" text,
+  "wdtk-id" text
+);
+\copy schemas_staging_local_authority from 'data/uk_local_authorities.csv' csv header;
+
+insert into schemas_local_authority (code, name, nice_name, nation, region)
+select
+  "gss-code",
+  "official-name",
+  "nice-name",
+  "nation",
+  "region" from schemas_staging_local_authority where "gss-code" is not null;
+
+drop table schemas_staging_local_authority;
 
 -- Authentication
 \copy authentication from 'data/authentication.csv' csv header;
--- tables/tbl_schemas_local_authority.sql
+++ tables/tbl_schemas_local_authority.sql
@@ -1,6 +1,9 @@
 create table schemas_local_authority (
   code character (9),
   name character varying (100),
+  nice_name character varying (100),
+  nation character varying (100),
+  region character varying (100),
   constraint pk_schemaslocalauthority_code primary key (code)
 );
 
