4eb4f96872b5b5a361ddfec85d7a25e3c283752f
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-04-03 21:19:10 +0100

    Starting on script to audit library web links

---
+++ scripts/remove_invalid_library_urls.py
@@ -0,0 +1,48 @@
+"""Removes invalid URLs from library CSV files"""
+
+import os
+from pathlib import Path
+import csv
+import urllib.request, urllib.error, urllib.parse
+
+FOLDER_NAME = Path("../data/schemas/libraries")
+
+
+def run():
+    """Runs the main script"""
+
+    folder = os.path.join(os.path.dirname(__file__), FOLDER_NAME)
+
+    for filename in os.listdir(folder):
+        if filename.endswith(".csv"):
+            print("Processing " + filename)
+            file_path = os.path.join(folder, filename)
+            with open(file_path, newline="") as csvfile:
+                reader = csv.DictReader(csvfile)
+                fieldnames = reader.fieldnames
+                rows = []
+                for row in reader:
+                    url = row["URL"]
+                    if url != "None" and url != "":
+                        print("Checking " + url)
+                        try:
+                            conn = urllib.request.urlopen(url)
+                        except urllib.error.HTTPError as e:
+                            row["URL"] = ""
+                        except urllib.error.URLError as e:
+                            row["URL"] = ""
+                        except InvalidURL as e:
+                            row["URL"] = ""
+                        else:
+                            print("Good URL")
+
+                    rows.append(row)
+
+            with open(file_path, "w", newline="") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                for row in rows:
+                    writer.writerow(row)
+
+
+run()
